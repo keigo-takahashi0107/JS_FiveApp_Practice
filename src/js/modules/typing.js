1d2976292546eb5056af83dc4a8616b95dbf6c2c
Reinosuke0 <131688383+Reinosuke0@users.noreply.github.com>
2023-09-19 02:53:49 +0900

    TypingApp Part4 Complete

--- src/js/modules/typing.js
+++ src/js/modules/typing.js
@@ -4,13 +4,15 @@ const titleTime = document.querySelector('#ty-title-time');
 const timer = document.querySelector("#ty-timer");
 const timeSelectEl = document.querySelector('.ty-time-select');
 const typing = document.querySelector("#typing");
-const backToStart = document.querySelector('#back-to-start');
+const backToStart = document.querySelector('#ty-back-to-start');
 const resultContainer = document.querySelector('#ty-result-container')
+const textarea = document.querySelector('#ty-textarea');
 
 let timelimit = 30;
 let remainingTime;
 let isActive = false;
 let isPlaying = false;
+let intervalId = null;
 
 timeSelectEl.addEventListener('change', () => {
     timelimit = timeSelectEl.value;
@@ -31,6 +33,24 @@ function start() {
     titleTime.textContent = timelimit;
     remainingTime = timelimit;
     timer.textContent = remainingTime;
+    textarea.focus();
+    textarea.disabled = false;
+
+    intervalId = setInterval(() => {
+        remainingTime -= 1;
+        timer.textContent = remainingTime;
+        if(remainingTime <= 0) {
+            showResult();
+        }
+    }, 1000)
+}
+
+function showResult() {
+    textarea.disabled = true;
+    clearInterval(intervalId);
+    setTimeout(() => {
+        resultContainer.classList.add('show');
+    }, 1000)
 }
 
 backToStart.addEventListener('click', () => {
