afb1babddc3f1d5b2c549383cd6e46a1e9b937d3
Reinosuke0 <131688383+Reinosuke0@users.noreply.github.com>
2023-09-21 04:19:12 +0900

    SlidePuzzleApp Part3 Complete

--- src/js/modules/slide.js
+++ src/js/modules/slide.js
@@ -3,16 +3,33 @@ const menu = document.querySelectorAll('.sp-menu > li');
 const backToMenu = document.querySelector('.sp-back-to-menu');
 const originalImage = document.querySelector('#sp-original-image');
 const showOriginalBtn = document.querySelector('#sp-show-original-btn');
+const screen = document.querySelector('.sp-screen');
 
 let level;
+let size;
+let orderedArray = [];
 const images = ['space', 'veges'];
 let selectedImage;
+const levelMap = {
+    easy: {grid:'auto auto', size:2},
+    medium: {grid:'auto auto auto', size:3},
+    difficult: {grid: 'auto auto auto auto', size:4}
+}
 
 menu.forEach(item => {
     item.addEventListener('click', () => {
         menuCover.classList.add('hide');
         level = item.dataset.level;
-        setOriginalImage();
+        size = levelMap[level].size;
+        orderedArray = [];
+        for(let x = 0; x < size; x++) {
+            for(let y = 0; y < size; y++) {
+                let tileXY = "" + x + y;
+                orderedArray.push(tileXY);
+            }
+        }
+        screen.style.gridTemplateColumns = levelMap[level].grid;
+        start();
     })
 })
 
@@ -33,3 +50,18 @@ showOriginalBtn.addEventListener('mouseover', () => {
 showOriginalBtn.addEventListener('mouseleave', () => {
     originalImage.classList.remove('show');
 })
+
+function renderTiles(arr) {
+    screen.innerHTML = '';
+    arr.forEach(tile => {
+        const div = document.createElement('div');
+        div.classList.add('sp-tile');
+        div.style.backgroundImage = `url(./images/slide_puzzle/${selectedImage}/${level}/tile${tile}.png)`;
+        screen.appendChild(div);
+    })
+}
+
+function start() {
+    setOriginalImage();
+    renderTiles(orderedArray);
+}
