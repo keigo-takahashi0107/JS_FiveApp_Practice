fe86a08ac9d0e97f5a6c2d0179ec2dfe5748f99b
Reinosuke0 <131688383+Reinosuke0@users.noreply.github.com>
2023-09-21 07:03:40 +0900

    SlidePuzzleApp Part5 Complete

--- src/js/modules/slide.js
+++ src/js/modules/slide.js
@@ -10,6 +10,7 @@ let size;
 let orderedArray = [];
 let hiddenTileIndex;
 let tilesArray = [];
+let tiles;
 const images = ['space', 'veges'];
 let selectedImage;
 const levelMap = {
@@ -79,6 +80,7 @@ function start() {
     setOriginalImage();
     tilesArray = generateShuffledArray(orderedArray);
     renderTiles(tilesArray);
+    updateScreen();
 }
 
 function generateShuffledArray(arr) {
@@ -90,4 +92,27 @@ function generateShuffledArray(arr) {
         shuffledArray[randomIndex] = tempValue;
     }
     return shuffledArray;
+}
+
+function updateScreen() {
+    tiles = document.querySelectorAll('.sp-tile');
+
+    function generateNewArray(arr, index, hiddenTileIndex) {
+        const tempValue = arr[index];
+        arr[index] = arr[hiddenTileIndex];
+        arr[hiddenTileIndex] = tempValue;
+        return arr;
+    }
+
+    function updateTiles(index) {
+        tilesArray = generateNewArray(tilesArray, index, hiddenTileIndex);
+        hiddenTileIndex = index;
+        renderTiles(tilesArray);
+    }
+
+    tiles.forEach((tile, index) => {
+        tile.addEventListener('click', () => {
+            updateTiles(index);
+        })
+    })
 }
